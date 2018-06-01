93914d2407545b7630e58fd7df3df22b063c0977
Durval Carvalho <dudurval2@gmail.com>
2018-06-01 20:45:49 -0300

    Versão-Final: Programa funcionando perfeitamente

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from random import randint
 from random import choice
+from os import system
 
 print("Bem vindo ao simulador do problema de Monty Hall!\n")
 
-#quant_simulacoes = int(input("Digite a quantidade de simulações que você deseja realizar\n"))
-quant_simulacoes = 1000
+quant_simulacoes = int(input("Digite a quantidade de simulações que você deseja realizar\n"))
+
+system('cls')
 
 portas = []
-for i in range(quant_simulacoes):  #Gera uma matriz com 3 portas em que somente uma delas está com o valor 1.
+for i in range(quant_simulacoes):                           #Gera uma matriz com 3 portas em que somente uma delas está com o valor 1.
         porta = [0, 0, 0]
         porta_correta = randint(0, 2)
         porta[porta_correta] = 1
 
-        portas.append(porta)  # Adciona a porta simulada em uma matriz
+        portas.append(porta)                                # Adciona a porta simulada em uma matriz
 
-acertos, erros = 0,0                    #Iniciando acerto e erros igual a 0
+acertos, erros = 0,0                                        #Iniciando acerto e erros igual a 0
 
-for i in range(quant_simulacoes):   #Escolhe um valor, compara com o da porta e contabiliza acertos e erros
+for i in range(quant_simulacoes):                           #Escolhe um valor, compara com o da porta e contabiliza acertos e erros
     tentativa = randint(0,2)
     if(portas[i][tentativa] == 1):
         acertos = acertos + 1
@@ -50,7 +52,7 @@ for i in range(quant_simulacoes):
 
     tentativa = choice(posibilidades)           #trocando a porta escolhida, pela porta remanecente
 
-    if(portas[0][tentativa] == 1):
+    if(portas[i][tentativa] == 1):
         acertos = acertos + 1
     else:
         erros = erros + 1
